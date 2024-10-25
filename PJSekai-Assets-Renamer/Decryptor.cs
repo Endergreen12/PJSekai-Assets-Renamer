2b9c76da03564a87a52a059194add57df1d8b889
暇人のEndergreen <90076182+Endergreen12@users.noreply.github.com>
2024-10-26 07:08:58 +0900

    Remove comment

--- PJSekai-Assets-Renamer/Decryptor.cs
+++ PJSekai-Assets-Renamer/Decryptor.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// https://github.com/Hazuki-san/SekaiDecryptor/blob/main/Program.cs
 namespace PJSekai_Assets_Renamer
 {
     internal class Decryptor
