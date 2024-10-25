2fe9721433ec9dc5d2b769e195900710ded2131a
暇人のEndergreen <endergreenjapan@gmail.com>
2024-10-25 19:29:51 +0900

    Implement feature

---
+++ PJSekai-Assets-Renamer/Decryptor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://github.com/Hazuki-san/SekaiDecryptor/blob/main/Program.cs
+namespace PJSekai_Assets_Renamer
+{
+    internal class Decryptor
+    {
+        public static byte[] DecryptAsset(byte[] sourceBytes)
+        {
+            byte[] decryptedBytes = sourceBytes;
+            for (int i = 0; i < 16; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (j < 5)
+                    {
+                        decryptedBytes[4 + i * 8 + j] = (byte)(~decryptedBytes[4 + i * 8 + j]);
+                    }
+                }
+            }
+
+            return decryptedBytes;
+        }
+    }
+}
--- PJSekai-Assets-Renamer/Program.cs
+++ PJSekai-Assets-Renamer/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MessagePack;
+using PJSekai_Assets_Renamer;
+using Sekai;
+
+// https://github.com/Hazuki-san/SekaiDecryptor/blob/main/Program.cs
+
+Console.WriteLine("PJSekai Assets Renamer");
+if (args.Length < 2 || args[0] == "--help")
+{
+    Console.WriteLine("Usage: PJSekai-Assets-Renamer.exe [Asset Data Folder Path] [Output Folder Path]");
+    Environment.Exit(0);
+}
+
+string dataFolderPath = args[0].Trim('"');
+
+// Load AssetBundleInfo
+var assetBundleInfo = MessagePackSerializer.Deserialize<AssetBundleInfo>( File.ReadAllBytes( Path.Combine(dataFolderPath, "AssetBundleInfo") ) );
+
+string outputPath = args[1].Trim('"');
+
+foreach(var pair in assetBundleInfo.bundles)
+{
+    var element = pair.Value;
+    try
+    {
+        string sourceDataPath = element.cacheDirectoryName == "" ? Path.Combine(dataFolderPath, element.cacheFileName) : Path.Combine(dataFolderPath, element.cacheDirectoryName, element.cacheFileName);
+        if (!File.Exists(sourceDataPath))
+        {
+            Console.WriteLine($"File {element.cacheFileName} does not exist");
+            continue;
+        }
+        byte[] sourceDataBytes = File.ReadAllBytes(sourceDataPath);
+
+        string outputFilePath = Path.Combine(outputPath, Path.Combine(element.bundleName.Split('/')));
+        Directory.CreateDirectory(Path.Combine(outputFilePath.Split(Path.DirectorySeparatorChar).SkipLast(1).ToArray()));
+        var fileStream = File.OpenWrite(outputFilePath);
+        var decryptedBytes = Decryptor.DecryptAsset(sourceDataBytes);
+        fileStream.Write(decryptedBytes, 4, decryptedBytes.Length - 4);
+        fileStream.Close();
+
+        Console.WriteLine(element.bundleName);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("An exception occoured");
+        Console.WriteLine("Bundle Name: " + element.bundleName);
+        Console.WriteLine("Exception Message: " + Environment.NewLine + ex.Message);
+        Console.WriteLine();
+    }
+}
